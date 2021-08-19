ea5e836efc47b500494600caebb42d7bdb9815aa
Christian Zigah <efochristian@outlook.com>
2021-08-19 18:46:31 +0100

    Revert Helmet settings

--- app.js
+++ app.js
@@ -93,23 +93,8 @@ initAuthMiddleware(app);
 
 // Helmet (no-cache)
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy());
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.expectCt());
-app.use(helmet.frameguard());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.hsts());
-app.use(helmet.ieNoOpen());
-app.use(helmet.noSniff());
-app.use(helmet.permittedCrossDomainPolicies());
-app.use(helmet.referrerPolicy());
-app.use(helmet.xssFilter());
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
-
+ 
+  
 // Morgan Logs
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, './logs/access.log'),
