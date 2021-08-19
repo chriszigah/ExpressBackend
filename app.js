62ebcb0bf2182b56a654b8402fa8a5e4780f4722
Christian Zigah <efochristian@outlook.com>
2021-08-19 18:37:17 +0100

    Security Helmet update

--- app.js
+++ app.js
@@ -93,6 +93,22 @@ initAuthMiddleware(app);
 
 // Helmet (no-cache)
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // Morgan Logs
 const accessLogStream = fs.createWriteStream(
